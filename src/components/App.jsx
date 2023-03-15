f6c14ed811d8612dbae377c2ef7faee74fc5e73e
Midland707 <rus14job@ukr.net>
2023-03-15 21:47:12 +0200

    refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import css from './App.module.css';
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const contactsStorage = localStorage.getItem('contactsStorage');
     const parsedContacts = JSON.parse(contactsStorage);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+    if (parsedContacts.length > 0) setContacts(parsedContacts);
+    console.log('contacts in Mount = ', contacts);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem(
-      'contactsStorage',
-      JSON.stringify(this.state.contacts)
-    );
-  }
+  useEffect(() => {
+    localStorage.setItem('contactsStorage', JSON.stringify(contacts));
+    console.log('contacts in Update = ', contacts);
+  }, [contacts]);
 
-  onSubmitForm = (data, resetForm) => {
+  const onSubmitForm = (data, resetForm) => {
     const { name } = data;
-    const alertState = this.state.contacts.find(
-      contact => contact.name === name
-    );
+    const alertState = contacts.find(contact => contact.name === name);
     if (!alertState) {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, data],
-      }));
+      setContacts(prevContacts => [...prevContacts, data]);
       resetForm.reset();
     } else alert(`${name} is already in contacts`);
   };
 
-  onFilterByName = eventFilter => {
+  const onFilterByName = eventFilter => {
     const filterValue = eventFilter.target.value.toLowerCase().trim();
-    this.setState({ filter: filterValue });
+    setFilter(filterValue);
   };
 
-  onClickDelete = idDelete => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== idDelete),
-    }));
+  const onClickDelete = idDelete => {
+    setContacts(contacts.filter(contact => contact.id !== idDelete));
   };
 
-  render() {
-    const filterContacts = this.state.contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(this.state.filter)
-    );
-    return (
-      <div className={css.section}>
-        <h1 className={css.title}>Phonebook</h1>
-        <ContactForm onSubmitForm={this.onSubmitForm} />
-        {this.state.contacts.length ? (
-          <h2 className={css.title}>Contacts</h2>
-        ) : null}
-        {this.state.contacts.length ? (
-          <Filter onFilterByName={this.onFilterByName} />
-        ) : null}
-        {this.state.contacts.length ? (
-          <ContactList
-            contacts={filterContacts}
-            // contacts={this.state.contacts}
-            onClickDelete={this.onClickDelete}
-          />
-        ) : null}
-      </div>
-    );
-  }
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLocaleLowerCase().includes(filter)
+  );
+  return (
+    <div className={css.section}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onSubmitForm={onSubmitForm} />
+      {contacts.length ? <h2 className={css.title}>Contacts</h2> : null}
+      {contacts.length ? <Filter onFilterByName={onFilterByName} /> : null}
+      {contacts.length ? (
+        <ContactList
+          contacts={filterContacts}
+          // contacts={this.state.contacts}
+          onClickDelete={onClickDelete}
+        />
+      ) : null}
+    </div>
+  );
 }
